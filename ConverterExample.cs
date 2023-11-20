9da06f2d2aeb73da1c147aa7884dcd5859e67c42
CMIT <CMIT@DESKTOP-SOP4B3I>
2023-11-20 13:26:20 +0530

    Add project files.

---
+++ ConverterExample.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ConverterEx
+{
+    public class ConverterExample : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            //throw new NotImplementedException();
+            switch(value.ToString())
+            {
+                    case "yes": return true;
+                    case "no": return false;
+            }
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            //throw new NotImplementedException();
+            if((bool)value == true)
+                return "yes";
+            else
+                return "no";
+        }
+    }
+}
